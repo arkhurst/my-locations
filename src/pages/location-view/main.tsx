a6f6b788b73de2ed52ea61b7b61edc133427d104
arkhurst <fiiarkhurst@gmail.com>
2022-02-18 22:40:07 +0000

    fixed single location page

--- src/pages/location-view/main.tsx
+++ src/pages/location-view/main.tsx
@@ -9,6 +9,7 @@ import { UrlParams } from "../location-edit/types";
 import { siteTitle } from "../../components/util";
 import { useAppDispatch, useAppSelector } from "../../services/broker/broker";
 import { getSingleLocation } from "../../services/broker/location-reducer";
+import { EmptyState } from "../../components/alerts";
 
 const pages: BreadCrumbProp[] = [
   { name: "Locations", href: LOCATIONS },
@@ -17,10 +18,6 @@ const pages: BreadCrumbProp[] = [
 function MainComponent() {
   const dispatch = useAppDispatch();
   const locationId: UrlParams = useParams();
-
-  const [latitude, setLatitude] = React.useState<number>(0);
-  const [longitude, setLogitude] = React.useState<number>(0);
-  const [address, setAddress] = React.useState<string>("");
 
   const selectedLocation = useAppSelector(
     (state) => state.locations.selectedValue
@@ -39,19 +36,6 @@ function MainComponent() {
     }
   }, [dispatch, locationId, selectedLocation]);
 
-  React.useEffect(() => {
-    if (selectedLocation.id) {
-      setAddress(selectedLocation?.address);
-      setLatitude(+selectedLocation?.coordinates?.latitude);
-      setLogitude(+selectedLocation?.coordinates?.longitude);
-    }
-  }, [
-    selectedLocation?.address,
-    selectedLocation?.coordinates?.latitude,
-    selectedLocation?.coordinates?.longitude,
-    selectedLocation.id,
-  ]);
-
   return (
     <>
       <div className="sticky top-0 z-40 ">
@@ -60,60 +44,72 @@ function MainComponent() {
       </div>
 
       <div className="mx-auto mt-6  w-full   max-w-7xl px-4 sm:px-4 lg:px-0 ">
-        <div className="grid  grid-cols-5 gap-4 sm:grid sm:grid-cols-5">
-          <div className="col-span-5 sm:col-span-5 lg:col-span-3">
-            <div
-              style={{
-                height: "58vh",
-              }}
-            >
-              <MapView lng={longitude} lat={latitude} address={address} />
-            </div>
-          </div>
+        {selectedLocation ? (
+          <>
+            <div className="grid  grid-cols-5 gap-4 sm:grid sm:grid-cols-5">
+              <div className="col-span-5 sm:col-span-5 lg:col-span-3">
+                <div
+                  style={{
+                    height: "58vh",
+                  }}
+                >
+                  <MapView
+                    lng={selectedLocation?.coordinates?.longitude}
+                    lat={selectedLocation?.coordinates?.latitude}
+                    address={selectedLocation?.address}
+                  />
+                </div>
+              </div>
 
-          <div
-            style={{
-              height: "58vh",
-            }}
-            className="col-span-5 mt-8 border border-gray-200 sm:col-span-5 sm:mt-8 lg:col-span-2 lg:mt-0"
-          >
-            <dl className="space-y-8 p-4 sm:space-y-4 sm:p-6">
-              <div>
-                <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">
-                  Name of location
-                </dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
-                  {selectedLocation?.name}
-                </dd>
+              <div
+                style={{
+                  height: "58vh",
+                }}
+                className="col-span-5 mt-8 border border-gray-200 sm:col-span-5 sm:mt-8 lg:col-span-2 lg:mt-0"
+              >
+                <dl className="space-y-8 p-4 sm:space-y-4 sm:p-6">
+                  <div>
+                    <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">
+                      Name of location
+                    </dt>
+                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
+                      {selectedLocation?.name}
+                    </dd>
+                  </div>
+                  <div>
+                    <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">
+                      Address
+                    </dt>
+                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
+                      {selectedLocation?.address}
+                    </dd>
+                  </div>
+                  <div>
+                    <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">
+                      Coordinates
+                    </dt>
+                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
+                      {selectedLocation?.coordinates?.latitude} ,{" "}
+                      {selectedLocation?.coordinates?.longitude}
+                    </dd>
+                  </div>
+                  <div>
+                    <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">
+                      Category
+                    </dt>
+                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
+                      {selectedLocation?.category}
+                    </dd>
+                  </div>
+                </dl>
               </div>
-              <div>
-                <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">
-                  Address
-                </dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
-                  {selectedLocation?.address}
-                </dd>
-              </div>
-              <div>
-                <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">
-                  Coordinates
-                </dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
-                  {selectedLocation?.coordinates?.latitude} ,{" "}
-                  {selectedLocation?.coordinates?.longitude}
-                </dd>
-              </div>
-              <div>
-                <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">
-                  Category
-                </dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
-                  {selectedLocation?.category}
-                </dd>
-              </div>
-            </dl>
-          </div>
-        </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <EmptyState model="location" onAdd={() => {}} />
+          </>
+        )}
       </div>
     </>
   );
